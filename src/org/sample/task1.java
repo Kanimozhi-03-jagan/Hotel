9e64a733eef51a457caa64ce3b4590abe7716a10
Kanimozhi-03-jagan <kanimozhijagan15@gmail.com>
2024-09-21 13:17:44 +0530

    new code

---
+++ src/org/sample/task1.java
@@ -0,0 +1,76 @@
+package org.sample;
+
+public class task1 {
+public static void main(String[] args) {
+	String s="java Programming launguage";
+	String s1="Java programming Launguage";
+
+	//find length
+	int e=s.length();
+	System.out.println(e);
+	
+	int  f = s.lastIndexOf('P');
+	System.out.println(f);
+/*for(int i=0;i<s.length();i++) {
+	char ch=s.charAt(i);
+	System.out.println(s.charAt(1));
+}
+		//convert uppercase
+	String r=s.toUpperCase();
+	System.out.println(r);
+	//convert lowercase
+	String u=s.toLowerCase();
+	System.out.println(u);
+	//character present in particular value
+	 char l = s.charAt( 3);
+	System.out.println(l);
+	//find index particular value
+	int  d = s.indexOf('r');
+	System.out.println(d);
+	find last index particular value
+	int  f = s.lastIndexOf('l');
+	System.out.println(f);
+	//check string strat with particular value
+	boolean  t = s.startsWith("java");
+	System.out.println(t);
+	//check string ends with particular vlaue
+	boolean  j = s.endsWith("launguage");
+	System.out.println(j);
+	//to chech empty or not
+	boolean  empty = s.isEmpty();
+	System.out.println(empty);
+	 //contains particular value
+	boolean contains = s.contains("gram");
+	System.out.println(contains);
+	//replace one char or word into another class
+	String replace = s.replace( 'o', 'l');
+	System.out.println( replace);
+	//replace all the one char all occurace char in to another char
+	String replaceAll = s.replaceAll( "java", "programming");
+	System.out.println(replaceAll);
+	//to strat with particular index value
+	String substring = s.substring( 4);
+	System.out.println(substring);
+	//to strats end with particular index value
+	String substringend = s.substring( 1, 8);
+	System.out.println(substringend);
+	//companre 2 sting and is case sensitive
+	boolean  z = s1.equals( s);
+	System.out.println(z);
+	//to compare 2 string its is not case sensitve
+	boolean ignore=s1.equalsIgnoreCase(s1);
+	System.out.println(ignore);
+	System.out.println(s1.equalsIgnoreCase(s));
+	//trim unwanted space remove
+	String trim = s.trim();
+	System.out.println(trim);*/
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
